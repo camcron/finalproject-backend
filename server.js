3f42dbd878924a343a04756edfbcecce4ccf305f
camcron <camillacronqvist@hotmail.com>
2023-05-30 22:28:58 +0200

    test for new deploy

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const apiKey = process.env.API_KEY
+const fetch = require('node-fetch');
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/odyssey";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,6 +23,25 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Final project!");
+});
+
+// Getting the lng, lat from the frontend to pass to PLaces API
+app.post('/api/places', async (req, res) => {
+  const { lng, lat } = req.body;
+  const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&type=tourist_attraction&radius=1500&key=${apiKey}`;
+
+  try {
+    const placesResponse = await fetch(placesUrl);
+    if (placesResponse.ok) {
+      const placesData = await placesResponse.json();
+      res.json(placesData);
+    } else {
+      throw new Error(`Google Places API error! Status: ${placesResponse.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
 });
 
 // Start the server
