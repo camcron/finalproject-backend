5204d02e81259d1bb6a1664700652f72c1e8bd92
Frid Matilda <matilda.x.frid@scania.com>
2023-06-08 19:55:17 +0200

    fixed the google places api a bit

--- Routes/google-api.js
+++ Routes/google-api.js
@@ -4,24 +4,29 @@ import fetch from 'node-fetch';
 const app = express();
 const router=express.Router()
 
-// Getting the lng, lat from the frontend to pass to PLaces API
+// Getting the lng, lat & type from the frontend to pass to PLaces API
 router.post('/api/places', async (req, res) => {
-    const { lng, lat, selectedFilter } = req.body;
-    const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&type=${selectedFilter}&radius=25000&key=${apiKey}`;
-  
-    try {
-      const placesResponse = await fetch(placesUrl);
-      if (placesResponse.ok) {
-        const placesData = await placesResponse.json();
-        res.json(placesData);
-      } else {
-        throw new Error(`Google Places API error! Status: ${placesResponse.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(error.message);
+  try {
+    const { lng, lat, type } = req.body;
+    const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&type=${type}&radius=25000&key=${apiKey}`;
+
+    const placesResponse = await fetch(placesUrl);
+
+    if (placesResponse.ok) {
+      const placesData = await placesResponse.json();
+      res.json(placesData);
+    } else {
+      throw new Error(`Google Places API error! Status: ${placesResponse.status}`);
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching places data",
+      error: error.message
+    });
+  }
+});
 
 export default router;
 
