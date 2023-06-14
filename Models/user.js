5864eeea58ae4975d7fe716c0f4d8adf5e95b723
camcron <camillacronqvist@hotmail.com>
2023-06-14 21:55:30 +0200

    added new default profilepic

--- Models/user.js
+++ Models/user.js
@@ -30,8 +30,8 @@ const UserSchema = new mongoose.Schema({
   },
   profilePicture: {
     type: String,
-    default: "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"
-    // data: Buffer, What does this mean??
+    // default: "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"
+    default: "https://www.flaticon.com/free-icon/woman_4140065?term=avatar&page=2&position=39&origin=tag&related_id=4140065"
   },
   profileInstagram: {
     type: String,
